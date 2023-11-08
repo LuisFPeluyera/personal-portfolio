b117991cb46fddf6bf31bd667f22c978d64a4d4b
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-11-07 19:00:07 -0600

    portfolio init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter as Router, Link, Routes, Route} from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
