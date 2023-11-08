745f917a5dd88623deedbfb707c53e57fe1033a1
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-11-07 21:04:45 -0600

    pages and navbar component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import {BrowserRouter as Router, Link, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Contact from "./pages/Contact";
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Portfolio</h1>
+
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home /> } />
+                <Route path="/about" element={<About /> } />
+                <Route path="/projects" element={<Projects /> } />
+                <Route path="/contact" element={<Contact /> } />
+
+            </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/Navbar.css'
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="toggleButton">
+                <button></button>
+            </div>
+            <div className="links">
+                <Link  to="/"> Home </Link>
+                <Link  to="/about" > About Me </Link>
+                <Link  to="/projects"> Projects </Link>
+                <Link  to="/contact" > Contact Me </Link>
+            </div>
+
+
+
+        </div>
+    )
+}
