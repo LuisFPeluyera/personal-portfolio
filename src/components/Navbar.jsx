8bd455f42b2f46a13ce97d430647a9db8b67bd65
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-11-08 00:04:14 -0600

    navbar styling added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import { Link, useLocation } from "react-router-dom";
 import '../styles/Navbar.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
+   const [ expandNavbar, setExpandNavbar ] = useState(false);
+   const location = useLocation();
+
+   useEffect(()=>{
+       setExpandNavbar(false)
+       console.log("useEffect worked");
+   },[location])
+
+
+
     return (
-        <div className="navbar">
+        <div className="navbar" id={ expandNavbar ? "open" : "close"}>
             <div className="toggleButton">
-                <button></button>
+                <button onClick={()=>{
+                    setExpandNavbar(!expandNavbar);
+                    console.log(expandNavbar);
+                }} >
+                    <MenuIcon />
+                </button>
             </div>
             <div className="links">
                 <Link  to="/"> Home </Link>
