f1859f5bd9dee10352bf925d003520909a529df7
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-11-09 09:37:35 -0600

    skills added to home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,29 @@ export default function Home() {
         <div className="home">
           <div className="about" >
             <h2>Hi, I'm Luis</h2>
-            <div className="prompt"> <p>Accomplished Army veteran and future Senior Web Developer</p></div>
-              <LinkedIn />
-              <GitHub />
-              <Email />
+            <div className="prompt">
+                <p>Accomplished Army veteran and future Senior Web Developer</p>
+                <LinkedIn />
+                <GitHub />
+                <Email />
+            </div>
           </div>
-          <div className="skills"></div>
+          <div className="skills">
+              <ol className="list">
+                  <li className="item">
+                      <h2>Front-End</h2>
+                      <span>ReactJs, Redux, NPM, HTML, CSS, Bootstrap, JQuery</span>
+                  </li>
+                  <li className="item">
+                      <h2>Back-End</h2>
+                      <span></span>
+                  </li>
+                  <li className="item">
+                      <h2>Languages</h2>
+                      <span>Javascript, Java, Solidity, Python</span>
+                  </li>
+              </ol>
+          </div>
         </div>
 
     );
