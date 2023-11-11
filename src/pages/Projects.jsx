119ac192ff8f333dc742387bddb41b5b24a51e8b
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-11-10 18:11:35 -0600

    project display component added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import ProjectDisplay from "./ProjectDisplay";
 
 export default function Projects() {
     return (
-        <h1>Projects</h1>
+        <>
+            <h1>Projects</h1>
+            <ProjectDisplay />
+        </>
     );
 }
