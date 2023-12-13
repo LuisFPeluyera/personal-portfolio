a8c9333c01fe351e1d4e9a7a06a424c305dc5b2c
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-12-12 21:58:07 -0600

    prepared for deployment

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -9,13 +9,13 @@ export const ProjectList = [
     {
         name: 'Hey Weather',
         image: HeyWeather,
-        skills: 'HTML, CSS Javascript, Mapbox API, OpenWeather API'
+        skills: 'This weather application is developed using HTML, CSS, and JavaScript. It leverages the OpenWeatherMap API to fetch weather information for a specified location and utilizes the MapBox API to showcase a map of that particular area.'
 
     },
     {
         name: 'Blue Bean Coffee Shop',
         image: CoffeeShop,
-        skills: 'HTML, CSS Javascript'
+        skills: 'This website serves as a straightforward yet inviting online presence for a charming coffee shop. Crafted using HTML, CSS, and JavaScript, it seamlessly blends aesthetics with functionality. Visitors can explore the coffee shop\'s offerings, navigate through a user-friendly interface, and experience the inviting ambiance—all brought to life through the synergy of web technologies.'
 
     },
     {
@@ -27,19 +27,13 @@ export const ProjectList = [
     {
         name: 'Basic Calculator',
         image: Calculator,
-        skills: 'HTML, CSS Javascript, JQuery'
-
-    },
-    {
-        name: 'Adlister project',
-        image: NotFound,
-        skills: 'some skills to update'
+        skills: 'This web application is an intuitive calculator designed for effortless calculations. Crafted with a combination of HTML for structure, CSS for styling, and JavaScript with jQuery for dynamic functionality, this calculator allows users to perform basic arithmetic operations with ease'
 
     },
     {
         name: 'React Wack a Mole',
         image: WhackAMole,
-        skills: 'HTML, CSS Javascript, REACT'
+        skills: 'Classic arcade whack a mole game developed using the dynamic trio of HTML, CSS, and React, this interactive experience puts a digital twist on the timeless Whack-a-Mole concept. Engage in a lively challenge as you aim to \'whack\' the virtual moles that pop up randomly on the screen. With its intuitive user interface and captivating visuals, this Whack-a-Mole game brings a dose of nostalgic enjoyment to the digital realm, making it a delightful and entertaining experience for players of all ages.'
 
     }
 ]
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,15 +9,15 @@ export default function About() {
                 <ol className="list">
                     <li className="item">
                         <h2>Front-End</h2>
-                        <span>ReactJs, Redux, NPM, HTML, CSS, Bootstrap, JQuery</span>
+                        <span>HTML, CSS, Javascript, Jquery, Bootstrap</span>
                     </li>
                     <li className="item">
                         <h2>Back-End</h2>
-                        <span>Java, Spring Boot, ...</span>
+                        <span>Java, Spring Boot, MySQL</span>
                     </li>
                     <li className="item">
-                        <h2>Languages</h2>
-                        <span>Javascript, Java, Solidity, Python</span>
+                        <h2>Exposure</h2>
+                        <span>Typescript, Solidity, Python</span>
                     </li>
                 </ol>
             </div>
